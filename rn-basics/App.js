348c1f1b97eca2c473803805af88a72c777b2e01
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-05-30 15:01:25 -0700

    Created react native app.
    Setting up environment.

---
+++ rn-basics/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Setting up environment to run applications in iOS and Android
+          simulators.
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
